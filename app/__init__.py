51178b1a966236f6d15d3f3d82a61b8cbe4df8f7
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-04 18:34:05 +0300

    initialize flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from config import config_options
 from flask_uploads import UploadSet, configure_uploads, IMAGES
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
-
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos', IMAGES)
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -17,8 +22,13 @@ def create_app(config_name):
     configure_uploads(app, photos)
 
     # initialize flask extensions
+    bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+
     # register Blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app 
 
