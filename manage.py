65791728e60223e6e3b6b40c5c9e5b7ce8504a3d
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-04 23:19:57 +0300

    remove userrole model

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import unittest
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User, Role, UserRole, Photo, Blog, Comment
+from app.models import User, Role, Photo, Blog, Comment
 from flask_migrate import Migrate, MigrateCommand
 
 # create app instance 
