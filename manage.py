e5d054beab793224786d40f32cf2ace58142bb3b
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-04 18:42:16 +0300

    add tests

--- manage.py
+++ manage.py
@@ -8,13 +8,13 @@ app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server', Server)
-# @manager.command
-# def test():
-#     '''
-#     run unittests
-#     '''
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity = 2).run(tests)
+@manager.command
+def test():
+    '''
+    run unittests
+    '''
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity = 2).run(tests)
 
 @manager.shell
 def make_shell_context():
