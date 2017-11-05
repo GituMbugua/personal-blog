997cc7714b1978f8a018b1efd390d79f13f92234
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-05 23:17:45 +0300

    configure flask mail

--- config.py
+++ config.py
@@ -8,6 +8,17 @@ class Config:
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USER_TLS = True
+    MAIL-USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
 class ProdConfig(Config):
     '''
     child configuration class for production
