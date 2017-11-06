e5949ec23b08e974f224c2ba39f38ca6e052d691
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-06 09:04:57 +0300

    remove mail

--- config.py
+++ config.py
@@ -16,8 +16,6 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USER_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
