f061eb4af47f66b3e6912e009f67600deb04baee
Gitu Mbugua <gmbugua38@gmail.com>
2017-11-07 10:17:09 +0300

    edit models

--- app/models.py
+++ app/models.py
@@ -38,6 +38,7 @@ class Role(db.Model):
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255), index = True)
+    users = db.relationship('User', backref = 'role', lazy = 'dynamic')
 
 class Photo(db.Model):
     __tablename__ = 'photos'
